2e4e4f40c6f20af514d95a3d97050153d921e7cd
mouahrara <mouahrara@users.noreply.github.com>
2025-09-24 23:21:33 +0200

    Add support for custom buildings

---
+++ InstantBuildingConstruction/ConsoleCommands.cs
@@ -0,0 +1,20 @@
+﻿using StardewModdingAPI;
+
+namespace InstantBuildingConstructionAndUpgrade
+{
+	public partial class ModEntry
+	{
+		internal void RegisterConsoleCommands()
+		{
+			SHelper.ConsoleCommands.Add("ibcu_reload", "Reapply modifications to building data and farmhouse renovation data.", IBCU_reload);
+		}
+
+		private void IBCU_reload(string command, string[] args)
+		{
+			SHelper.GameContent.InvalidateCache(asset => asset.Name.IsEquivalentTo("Data/Buildings"));
+			SHelper.GameContent.InvalidateCache(asset => asset.Name.IsEquivalentTo("Data/HomeRenovations"));
+			SHelper.GameContent.InvalidateCache(asset => asset.NameWithoutLocale.IsEquivalentTo("Strings/Locations"));
+			SMonitor.Log("Modifications to building data and farmhouse renovation data reapplied.", LogLevel.Info);
+		}
+	}
+}
