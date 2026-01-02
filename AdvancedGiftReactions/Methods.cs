f56f8a287ef1f0687883c021b8ffb3f1c0fe4ec2
aedenthorn <aedenthorn1@gmail.com>
2026-01-03 03:23:19 +0530

    etc

--- AdvancedGiftReactions/Methods.cs
+++ AdvancedGiftReactions/Methods.cs
@@ -7,28 +7,28 @@
             switch (taste)
             {
                 case 0:
-                    if (Config.LovedToLiked > -1 && num >= Config.LovedToLiked)
+                    if (Config.LovedToLiked > 0 && num >= Config.LovedToLiked)
                     {
                         taste = 2;
                         ModifyGiftTaste(ref taste, num - Config.LovedToLiked);
                     }
                     break;
                 case 2:
-                    if (Config.LikedToNeutral > -1 && num >= Config.LikedToNeutral)
+                    if (Config.LikedToNeutral > 0 && num >= Config.LikedToNeutral)
                     {
                         taste = 8;
                         ModifyGiftTaste(ref taste, num - Config.LikedToNeutral);
                     }
                     break;
                 case 8:
-                    if (Config.NeutralToDisliked > -1 && num >= Config.NeutralToDisliked)
+                    if (Config.NeutralToDisliked > 0 && num >= Config.NeutralToDisliked)
                     {
                         taste = 4;
                         ModifyGiftTaste(ref taste, num - Config.NeutralToDisliked);
                     }
                     break;
                 case 4:
-                    if (Config.DislikedToHated > -1 && num >= Config.DislikedToHated)
+                    if (Config.DislikedToHated > 0 && num >= Config.DislikedToHated)
                     {
                         taste = 6;
                     }
